ba97550faa4fae8e2da1f77a3be75a5927701539
whoisjones <jonas.golde@gmail.com>
2024-10-01 14:09:29 -0700

    change asknews path for processing

--- data/process_asknews.py
+++ data/process_asknews.py
@@ -7,11 +7,18 @@ def process_asknews(unprocessed_data_path: str):
 
     train_split = unprocessed_data["train"]
 
-    with open("/home/ec2-user/paper_data/train_datasets/asknews.json", "w") as f:
+    #with open("/home/ec2-user/paper_data/train_datasets/asknews.json", "w") as f:
+    #    json.dump(train_split, f)
+    
+    with open("/vol/tmp/goldejon/gliner/train_datasets/asknews.json", "w") as f:
         json.dump(train_split, f)
 
 
 if __name__ == "__main__":
+    #process_asknews(
+    #    unprocessed_data_path="/home/ec2-user/paper_data/train_datasets/asknews_unprocessed.json"
+    #)
+
     process_asknews(
-        unprocessed_data_path="/home/ec2-user/paper_data/train_datasets/asknews_unprocessed.json"
+        unprocessed_data_path="/vol/tmp/goldejon/gliner/train_datasets/asknews_unprocessed.json"
     )
